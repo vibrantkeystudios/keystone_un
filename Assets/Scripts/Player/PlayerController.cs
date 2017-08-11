b664477e1d60c0df33d0495d5ecbbe8843d6cb74
Developer5262 <dylan.fernandes00@gmail.com>
2017-08-10 23:32:45 -0400

    nav-mesh complete

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 6f;
+
+	Vector3 movement;
+	Rigidbody playerRigidbody;
+	int floorMask;
+	float camRayLength = 100f;
+
+	void Awake() {
+		 floorMask = LayerMask.GetMask("Floor");
+		 playerRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate() {
+		float v = Input.GetAxisRaw("Horizontal");
+		float h = Input.GetAxisRaw("Vertical");
+		Move(h, -v);
+		Turning();
+	}
+
+	void Move( float h, float v ) {
+		movement.Set(h, 0f, v);
+		movement = movement.normalized * speed * Time.deltaTime;
+		playerRigidbody.MovePosition(transform.position + movement);
+	}
+
+	void Turning() {
+		Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit floorHit;
+
+		if(Physics.Raycast(camRay, out floorHit, camRayLength, floorMask)) {
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			playerToMouse.y = 0f;
+			Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+			playerRigidbody.MoveRotation(newRotation);
+		}
+	}
+}
